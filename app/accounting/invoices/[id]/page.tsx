80f9021191c77ae4214e8a44b3fe0cb1412a630a
Mohamed Elsamahy <Mohamed.amir2022@feps.edu.eg>
2026-06-05 09:58:51 +0300

    fix(invoice-page): fix JSX syntax - modal was outside root div

--- app/accounting/invoices/[id]/page.tsx
+++ app/accounting/invoices/[id]/page.tsx
@@ -522,7 +522,6 @@ export default function InvoiceDetailPage() {
                     </table>
                 </div>
             )}
-        </div>
 
             {/* Payment Registration Modal */}
             {showPaymentModal && invoice && (
